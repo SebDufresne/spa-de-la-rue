4264df34c836ce2b9d02e181caaabb1933f206ee
Sebastien Dufresne <splitul@gmail.com>
2019-08-31 13:14:08 -0400

    Renamed app tp Application

--- client/src/components/__tests__/Application.test.js
+++ client/src/components/__tests__/Application.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Application from 'components/Application';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('Appointment', () => {
+  it('renders without crashing', async () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Application />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
 });
