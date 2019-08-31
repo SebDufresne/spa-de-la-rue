476e8c7ca8659faa86b1572895bce9a3b4a0564e
spiritxhx <spiritxhx@gmail.com>
2019-08-31 13:24:42 -0400

    modify the types for users

--- backend_graphql/data/types.js
+++ backend_graphql/data/types.js
@@ -6,16 +6,16 @@ const typeDefs = gql`
     users: [User]
   }
   type User {
-    id: Int
-    first_name: String
-    last_name: String
-    gender: String
+    id: ID
+    first_name: String!
+    last_name: String!
+    gender: String!
     contact_email: String!
-    contsct_phone: String
+    contact_phone: String
     contact_prefered: String
     description: String
     picture: String
-    total_hours: String
+    total_hours: Int
     password_hash: String
     is_admin: Boolean
     is_active: Boolean
