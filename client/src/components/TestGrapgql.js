2ba5830ceb5dacedb1506eb36fb4869d3c35d96d
spiritxhx <spiritxhx@gmail.com>
2019-08-31 14:22:09 -0400

    create test component for graphql

---
+++ client/src/components/TestGrapgql.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function TestGrapgql() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'index.scss';
-import Application from 'components/Application';
-import * as serviceWorker from 'serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "index.scss";
+import Application from "components/Application";
+import * as serviceWorker from "serviceWorker";
+import { ApolloProvider } from "react-apollo";
+import {TestGraphql} from 'components/testGrapgql';
 
-ReactDOM.render(<Application />, document.getElementById('root'));
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <Application />
+    <TestGraphql />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
