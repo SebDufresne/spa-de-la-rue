b2c9964299e250cbd6a3700fcf9f7b9bb3d9aebe
spiritxhx <spiritxhx@gmail.com>
2019-08-30 23:32:24 -0400

    recreate a brandnew backend server

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,28 +1,28 @@
-// const express = require('express');
-// const router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-// /* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render('index', { title: 'Express' });
-// });
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
 
-// module.exports = router;
+module.exports = router;
 
-const express = require("express");
-const graphqlHTTP = require("express-graphql");
-const bodyParser = require("body-parser");
-const { grapqlExpress, graphiqlExpress } = require("apollo-server-express");
-const { makeExecutableSchema } = require("graphql-tools");
-const { createServer } = require("http");
+// const express = require("express");
+// const graphqlHTTP = require("express-graphql");
+// const bodyParser = require("body-parser");
+// const { grapqlExpress, graphiqlExpress } = require("apollo-server-express");
+// const { makeExecutableSchema } = require("graphql-tools");
+// const { createServer } = require("http");
 
-const app = express();
+// const app = express();
 
-app.use(
-  "/graphql",
-  graphqlHTTP({
-    schema: MyGraphQLSchema,
-    graphiql: true
-  })
-);
+// app.use(
+//   "/graphql",
+//   graphqlHTTP({
+//     // schema: MyGraphQLSchema,
+//     graphiql: true
+//   })
+// );
 
-app.listen(4000);
+// app.listen(4000);
