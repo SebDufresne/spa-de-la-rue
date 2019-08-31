b21531dbe8ab40d8aec8c8a7a192a9f8ced3099e
Sebastien Dufresne <splitul@gmail.com>
2019-08-30 20:01:29 -0400

    Backend initialized

--- backend/db/knex_seeds/01_addUsers.js
+++ backend/db/knex_seeds/01_addUsers.js
@@ -1,13 +1,29 @@
+const faker = require('faker');
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
+  return Promise.all([
+    knex('users').del(),
+    knex.raw('ALTER SEQUENCE users_id_seq RESTART WITH 1'),
+    knex('users').then(function() {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('users').insert([
+        {
+          first_name: 'FirstName',
+          last_name: 'LastName',
+          gender: 'M',
+          contact_email:'test@test.com',
+          contact_phone:'15555555555',
+          contact_prefered:'email',
+          description: faker.lorem,
+          picture: '',
+          total_hours: 0,
+          password_hash:'$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.',
+          is_admin: true,
+          is_active: true,
+          is_disable: false,
+        },
       ]);
-    });
+    }),
+  ]);
 };
