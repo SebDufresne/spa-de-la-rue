f18106a634acadbe457cc3ad6d23e57da99ef3dd
Sebastien Dufresne <splitul@gmail.com>
2019-08-30 20:57:00 -0400

    Initialized Frontend

--- README.md
+++ README.md
@@ -9,6 +9,18 @@
 [dotenv](https://www.npmjs.com/package/dotenv)
 [nodemon](https://www.npmjs.com/package/nodemon)
 
-## Run procject (dev mode)
+## Run backend (dev mode)
 
+```javascript
 npm run dev
+```
+
+[API user database](http://localhost:3001/api/users)
+
+## Run frontend (dev mode)
+
+```javascript
+npm start
+```
+
+[webpage](http://localhost:3000)
---
+++ client/src/hooks/useApplicationData.js
@@ -0,0 +1,42 @@
+import React, {useEffect, useReducer} from 'react';
+import axios from 'axios';
+import { type } from 'os';
+
+const SET_USERS = 'SET_USERS';
+  
+const dataReducer = (state, action) =>{
+
+  const actions ={
+
+    SET_USERS: {
+      ...state,
+      users: action.users
+    }
+
+  }
+
+  if (!actions[action.type]) {
+    throw new Error('Type of action not found');
+  }
+  return actions[action.type];
+}
+
+const useApplicationData = () => {
+
+  const [state, dispatch] = useReducer(dataReducer, {users: []})
+
+  useEffect(() => {
+    axios
+      .get('/api/users')
+      .then(result => dispatch({type: SET_USERS, users: result.data}))
+      .catch(error => console.log(error))
+  }, [])
+
+	return {
+		state,
+		dispatch,
+	};
+
+}
+
+export default useApplicationData;
