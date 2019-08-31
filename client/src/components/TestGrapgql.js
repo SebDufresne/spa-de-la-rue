a24f635c7059cac83292c919724a42baca66da98
spiritxhx <spiritxhx@gmail.com>
2019-08-31 14:37:39 -0400

    add the client needed package for Apollo

--- client/src/components/TestGrapgql.js
+++ client/src/components/TestGrapgql.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import {Query} from 'react-apollo'
+import {gql} from 'apollo-boost'
+
+const Test_Query = gql`
+  {
+    user(id: 1) {
+      id, 
+      first_name, 
+      last_name
+    }
+  }
+`
 
 export default function TestGrapgql() {
   return (
-    <div>
-      
-    </div>
+    <Query query={Test_Query}>
+      {({data: {user}, loading})=>{
+        return <p>
+          {user.first_name}
+        </p>
+      }}
+    </Query>
   )
 }
--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,15 @@ import "index.scss";
 import Application from "components/Application";
 import * as serviceWorker from "serviceWorker";
 import { ApolloProvider } from "react-apollo";
-import {TestGraphql} from 'components/testGrapgql';
+import { HttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import TestGraphql from "components/testGrapgql";
+import { ApolloClient } from "apollo-client";
+
+const client = new ApolloClient({
+  link: new HttpLink({uri: 'http://localhost:4000/graphql'}), 
+  cache: new InMemoryCache()
+});
 
 ReactDOM.render(
   <ApolloProvider client={client}>
