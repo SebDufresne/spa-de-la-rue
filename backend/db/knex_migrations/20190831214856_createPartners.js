acb2c7b7154565f6586e068688c4633144c6dc35
Sebastien Dufresne <splitul@gmail.com>
2019-08-31 22:16:37 -0400

    Created Partners table

---
+++ backend/db/knex_migrations/20190831214856_createPartners.js
@@ -0,0 +1,22 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('partners', table => {
+    table.increments('id').primary();
+    table.string('name').notNullable();
+    table.text('description').notNullable();
+    table.integer('address_id');
+    table.string('picture').notNullable();
+    table.string('url').nullable();
+    table.string('private_contact_name').nullable();
+    table.string('private_phone_number').nullable();
+    table.string('private_email').nullable();
+    table.boolean('is_active').notNullable().defaultTo(true);
+    table.boolean('is_address_visible').notNullable().defaultTo(true);
+    table.timestamps(true,true);
+    table.foreign('address_id').references('id').inTable('addresses');
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('partners');
+};
