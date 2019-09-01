a7c9f1d408cba5fb436fbb4ed78be932fd63f28b
spiritxhx <spiritxhx@gmail.com>
2019-09-01 12:44:07 -0400

    add the id field for user, still working on debug

--- backend/data/resolvers/resolvers.js
+++ backend/data/resolvers/resolvers.js
@@ -1,13 +1,17 @@
 // Imports: database
 const db = require("../../database");
 // GraphQL: Resolvers
+const users = db.module.table("users");
+console.log('query: ', db.module.table("users"))
 
 const resolvers = {
   Query: {
-    users: async () => {
-      const users = await db.module.table("users");
-      // console.log("users: ", users);
+    // return all the users
+    users: () => {
       return users;
+    },
+    user: (root, args, context) => {
+      return db.module.from("users").where({ id: 1 });
     }
   }
 };
