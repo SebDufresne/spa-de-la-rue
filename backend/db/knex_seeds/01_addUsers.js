5c868115e67157e31ef9c9b6a9ee883b22e5201d
spiritxhx <spiritxhx@gmail.com>
2019-08-31 23:31:36 -0400

    create a second user

--- backend/db/knex_seeds/01_addUsers.js
+++ backend/db/knex_seeds/01_addUsers.js
@@ -23,6 +23,21 @@ exports.seed = function(knex) {
           is_admin: true,
           is_disable: false,
         },
+        {
+          first_name: 'bob',
+          last_name: 'chicken',
+          gender: 'F',
+          contact_email:'test2@test2.com',
+          contact_phone:'25555555555',
+          contact_prefered:'phone',
+          description: faker.lorem,
+          picture: 'images/avatars/missing_avatar.svg',
+          total_hours: 0,
+          password_hash:'$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.',
+          status: 'active',
+          is_admin: false,
+          is_disable: false,
+        },
       ]);
     }),
   ]);
