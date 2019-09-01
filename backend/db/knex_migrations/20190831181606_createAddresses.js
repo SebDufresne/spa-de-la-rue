ed5343248ed4f734f85483af3bec224f4e259fc1
Sebastien Dufresne <splitul@gmail.com>
2019-08-31 21:24:23 -0400

    Created and Seeded address table

---
+++ backend/db/knex_migrations/20190831181606_createAddresses.js
@@ -0,0 +1,17 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('addresses', table => {
+    table.increments('id').primary();
+    table.string('address1').notNullable();
+    table.string('address2');
+    table.string('city').notNullable();
+    table.string('state').notNullable();
+    table.string('zip').notNullable();
+    table.string('country').notNullable();
+    table.timestamps(true,true);
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('addresses');
+};
