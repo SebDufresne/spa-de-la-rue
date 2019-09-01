dde31e225304baf36ff5f48fc8bb13e70694eb8a
spiritxhx <spiritxhx@gmail.com>
2019-09-01 15:35:28 -0400

    create a basic nav framework

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
 
 export default function Navbar() {
   return (
     <React.Fragment>
-      Hello form Navbar
+      <nav className="navbar navbar-expand-lg navbar-expand-md fixed-top navbar-light bg-light">
+        <div className="navbar nav">
+          <a className="Nav-brand" href="/">
+            Logo
+          </a>
+
+          <li className="collapse navbar-collapse" id="collapsibleNavbar">
+            <a className="nav-item nav-link">About us</a>
+            <a className="nav-item nav-link">News</a>
+          </li>
+        </div>
+      </nav>
     </React.Fragment>
-  )
+  );
 }
