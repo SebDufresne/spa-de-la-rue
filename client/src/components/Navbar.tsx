2710e436d2bf65f5c1fc5cb27b8e980e64c0830b
spiritxhx <spiritxhx@gmail.com>
2019-09-01 16:07:31 -0400

    create Navbar.scss and make the navbar display better

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,19 +1,25 @@
 import React from "react";
+import "components/Navbar.scss";
 
 export default function Navbar() {
   return (
     <React.Fragment>
-      <nav className="navbar navbar-expand-lg navbar-expand-md fixed-top navbar-light bg-light">
-        <div className="navbar nav">
-          <a className="Nav-brand" href="/">
-            Logo
-          </a>
+      <nav className="navbar navbar-expand-lg navbar-expand-md fixed-top navbar-light bg-light nav justify-content-between">
+        <a className="Nav-brand" href="/">
+          Logo
+        </a>
 
-          <li className="collapse navbar-collapse" id="collapsibleNavbar">
-            <a className="nav-item nav-link">About us</a>
-            <a className="nav-item nav-link">News</a>
-          </li>
-        </div>
+        <li
+          className="collapse navbar-collapse d-flex justify-content-end"
+          id="collapsibleNavbar"
+        >
+          <a className="nav-item nav-link" href="/">
+            About us
+          </a>
+          <a className="nav-item nav-link" href="/">
+            News
+          </a>
+        </li>
       </nav>
     </React.Fragment>
   );
