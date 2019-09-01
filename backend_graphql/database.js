5871a0e70fbc196a757916198e1d4612dc8ed2e1
spiritxhx <spiritxhx@gmail.com>
2019-08-31 20:40:55 -0400

    confirm the deletion

--- backend_graphql/database.js
+++
@@ -1,5 +0,0 @@
-const knex = require("knex")(
-  require("./knexfile")[process.env.NODE_ENV || "development"]
-);
-
-exports.module = knex;
--- backend_graphql/db/knex_migrations/20190831181606_createPartners.js
+++
@@ -1,6 +0,0 @@
-
-exports.up = function(knex) {
-};
-
-exports.down = function(knex) {
-};
