bc070ef60d4af2327a41ee622d38a54786d45238
spiritxhx <spiritxhx@gmail.com>
2019-09-02 18:30:33 -0400

    added back the placeholder and label props, cannot just use name and type

--- client/src/components/form/SmallForm.tsx
+++ client/src/components/form/SmallForm.tsx
@@ -3,17 +3,19 @@ import React from "react";
 interface formInfo{
   type: string
   name: string
+  placeholder: string
+  label: string 
 }
 
 export default function HalfForm(props: formInfo) {
   return (
     <div className="form-group col-md-4">
-      <label>{props.name}</label>
+      <label>{props.label}</label>
       <input
         type={props.type}
         className="form-control"
         name={props.name}
-        placeholder={props.name}
+        placeholder={props.placeholder}
       />
     </div>
   );
--- client/src/components/form/volunteer/CreateVolunteer.tsx
+++ client/src/components/form/volunteer/CreateVolunteer.tsx
@@ -1,12 +1,37 @@
 import React from "react";
 import SmallForm from "../SmallForm";
+import LongForm from "../LongForm";
 
 export default function CreateVolunteer() {
   return (
     <form>
       <div className="form-row">
-        <SmallForm name="email" type="email" />
-        <SmallForm name="password" type="password" />
+        <SmallForm
+          name="firstName"
+          type="name"
+          placeholder="First name"
+          label="First name"
+        />
+        <SmallForm
+          name="lastName"
+          type="name"
+          placeholder="Last name"
+          label="Last name"
+        />
+      </div>
+      <div className="form-row">
+        <SmallForm
+          name="password"
+          type="password"
+          placeholder="Password"
+          label="Password"
+        />
+        <SmallForm
+          name="passwordConformation"
+          type="password"
+          placeholder="Conform your password"
+          label="Confirm your password"
+        />
       </div>
     </form>
   );
