f8ae1d4c073e8587ec2cbfca395ecbad2c98a8eb
spiritxhx <spiritxhx@gmail.com>
2019-09-01 20:25:44 -0400

    trying the fix the navbar toggle

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -3,36 +3,46 @@ import "components/Navbar.scss";
 
 export default function Navbar() {
   return (
-    <React.Fragment>
-      <nav className="navbar navbar-expand-lg navbar-expand-md navbar-expand-sm fixed-top navbar-light bg-light justify-content-between nav">
-        <a className="Nav-brand" href="/">
-          Logo
-        </a>
-        <div className="d-flex">
-          <li className="collapse navbar-collapse " id="collapsibleNavbar">
-            <a className="nav-link" href="/">
+    <nav className="navbar navbar-expand-lg navbar-expand-md navbar-expand-sm fixed-top navbar-light bg-light justify-content-between nav">
+      <a className="navbar-brand" href="#">
+        Logo
+      </a>
+      <div className="d-flex">
+        <ul className="collapse navbar-collapse" id="collapsibleNavbar">
+          <li className="nav-item">
+            <a className="nav-link" href="#">
               About us
             </a>
-            <a className="nav-link" href="/">
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
               News
             </a>
-            <a className="nav-link" href="/">
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
               Press
             </a>
           </li>
-          <a className="nav-link" href="/">
+        </ul>
+        <li className="nav-item">
+          <a className="nav-link" href="#">
             Support Us
           </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#collapsibleNavbar"
-          >
-            <span className="navbar-toggler-icon navbar-inverse"></span>
-          </button>
-        </div>
-      </nav>
-    </React.Fragment>
+        </li>
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#collapsibleNavbar"
+          aria-controls="collapsibleNavbar"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+      </div>
+    </nav>
   );
 }
