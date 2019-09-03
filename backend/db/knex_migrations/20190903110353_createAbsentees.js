e4f0314790c6901aa1f4162cd4ac98f7e201b6ee
Sebastien Dufresne <splitul@gmail.com>
2019-09-03 11:13:39 -0400

    Created Absentees table

---
+++ backend/db/knex_migrations/20190903110353_createAbsentees.js
@@ -0,0 +1,16 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('absentees', table => {
+    table.increments('id').primary();
+    table.integer('clinic_id');
+    table.integer('volunteer_id');
+    table.boolean('is_absent');
+    
+    table.foreign('clinic_id').references('id').inTable('clinics');
+    table.foreign('volunteer_id').references('id').inTable('users');
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('absentees');
+};
