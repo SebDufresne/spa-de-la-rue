6e845750d87131a834ca6064f1338632237b8f68
spiritxhx <spiritxhx@gmail.com>
2019-09-03 11:04:00 -0400

    add an optional value for SmallInput in preparing for editing data

--- client/src/components/form/SmallInput.tsx
+++ client/src/components/form/SmallInput.tsx
@@ -1,22 +1,37 @@
 import React from "react";
 
-interface formInfo{
-  type: string
-  name: string
-  placeholder: string
-  label: string 
+interface formInfo {
+  type: string;
+  name: string;
+  placeholder: string;
+  label: string;
+  value?: string;
 }
 
 export default function SmallInput(props: formInfo) {
-  return (
-    <div className="form-group col-md-6">
-      <label>{props.label}</label>
-      <input
-        type={props.type}
-        className="form-control"
-        name={props.name}
-        placeholder={props.placeholder}
-      />
-    </div>
-  );
+  if (props.value) {
+    return (
+      <div className="form-group col-md-6">
+        <label>{props.label}</label>
+        <input
+          type={props.type}
+          className="form-control"
+          name={props.name}
+          placeholder={props.placeholder}
+        />
+      </div>
+    );
+  } else {
+    return (
+      <div className="form-group col-md-6">
+        <label>{props.label}</label>
+        <input
+          type={props.type}
+          className="form-control"
+          name={props.name}
+          value={props.value}
+        />
+      </div>
+    );
+  }
 }
