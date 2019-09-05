646a39f7e277a28e2f5433eeceb68ae6c352c5b2
spiritxhx <spiritxhx@gmail.com>
2019-09-04 21:27:57 -0400

    add the blank target so that follow button will open in a new tag

--- client/src/components/sponsors/Sponsor.tsx
+++ client/src/components/sponsors/Sponsor.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React from "react";
 
 interface sponsorDetails {
-  name: string,
-  picture_url: string,
-  description: string,
-  sponsor_url: string
+  name: string;
+  picture_url: string;
+  description: string;
+  sponsor_url: string;
 }
 
 export default function Sponsor(props: sponsorDetails) {
@@ -12,15 +12,18 @@ export default function Sponsor(props: sponsorDetails) {
 
   return (
     <main className="appointment__card appointment__card--error">
-      <img
-        className="appointment__error-close"
-        src={picture_url}
-        alt={name}
-      />
+      <img className="appointment__error-close" src={picture_url} alt={name} />
       <section className="">
         <h1>{name}</h1>
         <p>{description}</p>
-        <a href={sponsor_url} className="btn btn-primary">Follow them</a>
+        <a
+          href={sponsor_url}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="btn btn-primary"
+        >
+          Follow them
+        </a>
       </section>
     </main>
   );
