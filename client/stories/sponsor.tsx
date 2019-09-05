46d32e4f8beb763c69e6e20abd9192fb13326cdd
Sebastien Dufresne <splitul@gmail.com>
2019-09-04 22:54:33 -0400

    Standardized story book

--- client/stories/sponsor.tsx
+++ client/stories/sponsor.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { Button } from '@storybook/react/demo';
-import { action } from '@storybook/addon-actions';
-
 
 import Sponsor from "../src/components/sponsors/Sponsor";
 import SponsorList from "../src/components/sponsors/SponsorList";
