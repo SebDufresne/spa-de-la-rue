3b28ba17327ba64e9621302e4860eb0caa56ef46
Sebastien Dufresne <splitul@gmail.com>
2019-09-04 20:59:28 -0400

    Fixed conflict on master

--- client/src/components/navbar/Logo.tsx
+++ client/src/components/navbar/Logo.tsx
@@ -9,14 +9,7 @@ export default function Logo(props: logo) {
   const {logo_url} = props;
   return (
     <div className="navbar-brand">
-<<<<<<< HEAD
-      <Link to="/">
-        Logo
-        <img alt="Logo" />
-      </Link>
-=======
       <Link to="/"><img alt="Logo" src={logo_url} height="150" width="150" /></Link>
->>>>>>> master
     </div>
   );
 }
