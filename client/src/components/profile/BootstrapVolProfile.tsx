4c12f3a14d7b9d2a125b8c4b7709ba9c777125cd
spiritxhx <spiritxhx@gmail.com>
2019-09-07 18:19:39 -0400

    create the user profile using react bootstrap

--- client/src/components/profile/BootstrapVolProfile.tsx
+++ client/src/components/profile/BootstrapVolProfile.tsx
@@ -1,15 +1,10 @@
 import React, { useState } from "react";
-import { Form, Col } from "react-bootstrap";
+import { Form, Col, Row, Button } from "react-bootstrap";
 import { gql } from "apollo-boost";
 import { useQuery } from "@apollo/react-hooks";
 
 interface profileInfo {
-  first_name?: string;
-  last_name?: string;
-  gender?: string;
-  description?: string;
-  contact_phone?: string;
-  contact_email?: string;
+  contact_email: string;
 }
 
 const GET_VOLUNTEER_PROFILE = gql`
@@ -56,8 +51,54 @@ export default function BootstrapVolProfile(props: profileInfo) {
             placeholder="First Name"
             defaultValue={data.user.first_name}
           />
+          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+        </Form.Group>
+        <Form.Group as={Col} md="4" controlId="validation2">
+          <Form.Label>Last Name</Form.Label>
+          <Form.Control
+            required
+            type="text"
+            placeholder="Last Name"
+            defaultValue={data.user.last_name}
+          />
+          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
         </Form.Group>
       </Form.Row>
+      <Form.Row>
+        <Form.Group as={Col} md="8" controlId="validation3">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            required
+            type="text"
+            placeholder="Email"
+            defaultValue={data.user.contact_email}
+          />
+          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+        </Form.Group>
+      </Form.Row>
+      <Form.Row>
+        <Form.Group as={Col} md="4" controlId="validation4">
+          <Form.Label>Phone number</Form.Label>
+          <Form.Control
+            required
+            type="text"
+            placeholder="Phone number"
+            defaultValue={data.user.contact_phone}
+          />
+          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+        </Form.Group>
+      </Form.Row>
+      <Form.Row>
+        <Form.Group as={Col} md="8" controlId="validation5">
+          <Form.Label>Why you are interested</Form.Label>
+          <Form.Control
+            as="textarea"
+            rows="3"
+            defaultValue={data.user.description}
+          />
+        </Form.Group>
+      </Form.Row>
+      <Button type="submit">Submit form</Button>
     </Form>
   );
 }
