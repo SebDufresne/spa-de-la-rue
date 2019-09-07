7c4cfcc38e40470e17202c1f0c303354c02d5e5b
spiritxhx <spiritxhx@gmail.com>
2019-09-07 01:40:54 -0400

    add the drop down menu in table, but cannot click it at this time

--- client/src/components/admin/VolunteerItem.tsx
+++ client/src/components/admin/VolunteerItem.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import Table from "react-bootstrap";
+import { DropdownButton } from "react-bootstrap";
+import { Dropdown } from "react-bootstrap";
 
 interface volunteerItem {
   id: number;
@@ -15,7 +16,17 @@ export default function VolunteerItem(props: volunteerItem) {
       <td>{props.id}</td>
       <td>{props.first_name}</td>
       <td>{props.last_name}</td>
-      <td>{props.status}</td>
+      <td>
+        <DropdownButton
+          id="dropdown-item-button"
+          title={props.status}
+          variant={props.status === "active" ? "success" : "warning"}
+        >
+          <Dropdown.Item as="button">Enable</Dropdown.Item>
+          <Dropdown.Item as="button">Disable</Dropdown.Item>
+          <Dropdown.Item as="button">Reject</Dropdown.Item>
+        </DropdownButton>
+      </td>
     </tr>
   );
 }
