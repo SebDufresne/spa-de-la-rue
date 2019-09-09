b1df6ea05f675e3dc57a2a89a713edaa28060774
spiritxhx <spiritxhx@gmail.com>
2019-09-08 23:40:31 -0400

    add style sheet for calendar

--- client/src/components/Calendar.tsx
+++ client/src/components/Calendar.tsx
@@ -1,33 +1,36 @@
-import React from 'react';
-import MonthView from 'react-calendar';
-import moment from 'moment';
+import React from "react";
+import MonthView from "react-calendar";
+import moment from "moment";
+import './Calendar.scss';
 
 interface scheduleItem {
-  dates: string[],
-  class: string,
+  dates: string[];
+  class: string;
 }
 
 interface scheduleDetails {
-  schedule: scheduleItem[]
+  schedule: scheduleItem[];
 }
 
 export default function displayCalendar(props: scheduleDetails) {
   const { schedule } = props;
 
-  const dateToClass = (classList : scheduleItem[], date: Date) => {
+  const dateToClass = (classList: scheduleItem[], date: Date) => {
     for (const clinic of classList) {
-      const dateList = clinic.dates.map(e => moment(parseInt(e)).format('YYYY-MM-DD'));
-      if (dateList.includes(moment(date).format('YYYY-MM-DD'))) {
+      const dateList = clinic.dates.map(e =>
+        moment(parseInt(e)).format("YYYY-MM-DD")
+      );
+      if (dateList.includes(moment(date).format("YYYY-MM-DD"))) {
         return clinic.class;
       }
     }
     return null;
-  }
+  };
 
   return (
     <MonthView
-        calendarType = 'US'
-        tileClassName = {({ date }) =>  dateToClass(schedule, date) }
+      calendarType="US"
+      tileClassName={({ date }) => dateToClass(schedule, date)}
     />
   );
 }
