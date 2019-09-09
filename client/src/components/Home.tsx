f9911294fd460e304e644b6c8deb084506fb4639
spiritxhx <spiritxhx@gmail.com>
2019-09-08 21:56:59 -0400

    make the picture in home page take the whole screen

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,19 +1,24 @@
-import React from 'react'
+import React from "react";
 
 export default function Home() {
   return (
-    <div className="container text-center">
-    <h1>Welcome to Spa de la rue!!</h1>
-    <img
-        className="appointment__error-close"
+    <div className="text-center">
+      <img
+        className="appointment__error-close img-fluid"
         src="/images/assets/site_fontPage.png"
         alt="Spa de la Rue"
       />
-<blockquote className="blockquote">
-  <p className="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
-  <footer className="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
-</blockquote>
+      <h1>Welcome to Spa de la rue!!</h1>
+      <blockquote className="blockquote">
+        <p className="mb-0">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+          posuere erat a ante.
+        </p>
+        <footer className="blockquote-footer">
+          Someone famous in <cite title="Source Title">Source Title</cite>
+        </footer>
+      </blockquote>
       <h2>Our Volunteers</h2>
     </div>
-  )
+  );
 }
