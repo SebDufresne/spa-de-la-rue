446e643e283a3ca501340aa5e4919d4107d76389
spiritxhx <spiritxhx@gmail.com>
2019-09-10 17:00:52 -0400

    fix the social media bar to the left

--- client/src/components/SocialMedia.tsx
+++ client/src/components/SocialMedia.tsx
@@ -1,24 +1,17 @@
-import React from 'react';
-import { SocialIcon } from 'react-social-icons';
+import React from "react";
+import { SocialIcon } from "react-social-icons";
+import "./SocialMedia.scss";
 
 interface propsType {
-  [key:string]: string[]
+  [key: string]: string[];
 }
 
-export default function SocialMedia({props}: propsType) {
-    const socialMediaList = props.map((url, index) => {
-  
-        return (
-          <SocialIcon
-          key={index}
-          url = {url}
-        />
-        )
-      });
-  
-    return (
-      <section className="mediaBar" >
-        <ul>{socialMediaList}</ul>
-      </section>
-    );
+export default function SocialMedia({ props }: propsType) {
+  const socialMediaList = props.map((url, index) => {
+    return <SocialIcon key={index} url={url} />;
+  });
+
+  return (
+    <section className="mediaBar d-flex flex-column social-media">{socialMediaList}</section>
+  );
 }
