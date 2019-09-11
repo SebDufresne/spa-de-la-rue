f957354bbaf6a2e54a27b456405807ba32395f21
spiritxhx <spiritxhx@gmail.com>
2019-09-11 11:03:49 -0400

    change the layout of the partner

--- client/src/components/partners/Partner.tsx
+++ client/src/components/partners/Partner.tsx
@@ -1,21 +1,23 @@
-import React from 'react';
+import React from "react";
 
-import { PartnerSummary } from './types';
+import { PartnerSummary } from "./types";
 
 export default function Partner(partners: PartnerSummary) {
   const { name, description, picture_url } = partners;
 
   return (
     <main className="appointment__card appointment__card--error">
-      <img
-        className="appointment__error-close"
-        src={picture_url}
-        alt={name}
-      />
-      <section className="">
-        <h2>{name}</h2>
-        <p>{description}</p>
-      </section>
+      <div className="row my-5">
+        <img
+          className="appointment__error-close col-md-6"
+          src={picture_url}
+          alt={name}
+        />
+        <div className="col-md-6">
+          <h2>{name}</h2>
+          <p className="text-left">{description}</p>
+        </div>
+      </div>
     </main>
   );
 }
